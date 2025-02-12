f6964e058105df3f76702c8c1f0c5051b0d37964
An3step <an3step@gmail.com>
2025-02-12 21:37:32 +0300

    Update Fibbonacci.cpp

--- Fibbonacci/Fibbonacci.cpp
+++ Fibbonacci/Fibbonacci.cpp
@@ -1,12 +1,9 @@
-﻿// Fibbonacci.cpp: определяет точку входа для приложения.
-//
-
 #include "Fibbonacci.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	cin >> n;
+	cout << Fibonacci(n);
 }
